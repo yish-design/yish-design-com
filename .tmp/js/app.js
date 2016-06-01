c3ebf2de0ec463ed40b17b91d1d9df91d4f9c012
zanjs <root@zanjs.com>
2016-06-01 18:28:42 +0800

    new bug :pensive:

--- .tmp/js/app.js
+++ .tmp/js/app.js
@@ -180,26 +180,52 @@ function workLayoutTwo(ele){
     
     var f1 = glw*2;
     
+    var onPaiLen = 5;
+    
     var work = ele;
     
-    var index = work.index();
+    var imgBox = work.children();
+    var imgBoxLength = imgBox.length;
     
-    var cNum = index % 2;
+    var flooLeng = Math.ceil(imgBoxLength/onPaiLen);
     
-  
+    work.css({'height':flooLeng * f1});
     
-    var arrCh = work.children();
+   imgBox.each(function(index){
+       
+      var _t = $(this);
+      var _index = _t.index();
     
-    work.css("height",f1);
+      var onPai = Math.ceil((_index+1)/onPaiLen);
+      
+      var isL = onPai % 2;
+      
+      var _w = glw;
+      var _h = glw;
+      
+      var isBig = _index % onPaiLen == 0 ? true : false;
+       
+       if(isBig){
+           
+           _w = f1;
+           _h = f1;
+           
+       } 
+       
+       _t.animate({'width':_w,'height':_h,'left':glw*2},50)
+       
+       
+   });
+
     
-    var isL = cNum == 0 ? 0 : f1;
+
     
-    $(".imgbox").css({'position':'absolute'})
-    arrCh.eq(0).css({'position':'absolute','left':0+isL,'width':f1,'height':f1})
-    arrCh.eq(1).animate({'width':glw,'height':glw,'left':glw*2 - isL},50)
-    arrCh.eq(2).animate({'width':glw,'height':glw,'left':glw*3 - isL},50)
-    arrCh.eq(3).animate({'width':glw,'height':glw,'left':glw*2 - isL,'top':glw},50)
-    arrCh.eq(4).animate({'width':glw,'height':glw,'left':glw*3 - isL,'top':glw},50)
+    // $(".imgbox").css({'position':'absolute'})
+    // arrCh.eq(0).css({'position':'absolute','left':0+isL,'width':f1,'height':f1})
+    // arrCh.eq(1).animate({'width':glw,'height':glw,'left':glw*2 - isL},50)
+    // arrCh.eq(2).animate({'width':glw,'height':glw,'left':glw*3 - isL},50)
+    // arrCh.eq(3).animate({'width':glw,'height':glw,'left':glw*2 - isL,'top':glw},50)
+    // arrCh.eq(4).animate({'width':glw,'height':glw,'left':glw*3 - isL,'top':glw},50)
     
     
     
@@ -224,11 +250,12 @@ function ifworkLayout() {
     var winW = window.innerWidth;
     
     if(winW > 1023){
+        // clearStyle();
         $(".work-floor").each(function(i){
    			 workLayoutTwo($(this))
  		});
-    }else if(winW > 768 && winW < 1023){
-        workLayoutIpad();
+    }else if(winW >= 768 && winW <= 1023){
+        workLayoutIpad(); 
        
     }else{
         clearStyle();
