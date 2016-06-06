7e615529446524a9a3e87219066fb73d823371da
zanjs <root@zanjs.com>
2016-06-06 10:37:13 +0800

    There is a great different between exposure of the mind and that of the body :feelsgood:

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,36 @@
  * 2016年04月29日10:51:34
  * Julian
  */
-import gulp from 'gulp';
-import uglify from 'gulp-uglify';
-import sass from 'gulp-sass';
-import prefix from 'gulp-autoprefixer';
-import rename from 'gulp-rename';
-import minifycss from 'gulp-minify-css';
+import gulp         from 'gulp';
+import uglify       from 'gulp-uglify';
+import sass         from 'gulp-sass';
+import prefix       from 'gulp-autoprefixer';
+import rename       from 'gulp-rename';
+import minifycss    from 'gulp-minify-css';
 
-import concat from 'gulp-concat';
-import notify from 'gulp-notify';
-import babel from 'gulp-babel';
-import browserSync from 'browser-sync';
-import saveLicense from 'uglify-save-license';
+import concat       from 'gulp-concat';
+import notify       from 'gulp-notify';
+import babel        from 'gulp-babel';
+import browserSync  from 'browser-sync';
+import saveLicense  from 'uglify-save-license';
+
+import header       from 'gulp-header';
+import pkg          from './package.json';
+
 const  reload  = browserSync.create().reload;
 const buildSrc = './build/';
 const minjs = 'app.js';
 
+
+const banner = [
+  '/*! ',
+    '<%= pkg.name %> ',
+    'v<%= pkg.version %> | ',
+    `(c) ${new Date()} <%= pkg.author %> |`,
+    ' <%= pkg.homepage %>',
+  ' */',
+  '\n'
+].join('');
 
 
 gulp.task('sass', () => {
@@ -26,6 +40,7 @@ gulp.task('sass', () => {
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8'], { cascade: true }))
         .pipe(rename('app.css'))
 		.pipe(minifycss())
+        .pipe(header(banner, { pkg }))
         .pipe(gulp.dest('./build/css/'))
         .pipe(browserSync.reload({stream:true}));
 });
@@ -34,10 +49,8 @@ gulp.task('scripts',() => {
     return gulp.src('./src/js/*.js')
         .pipe(concat(minjs))
         .pipe(gulp.dest('./.tmp/js'))
-        .pipe(uglify({
-      mangle: false,
-      preserveComments: saveLicense
-    }))
+        .pipe(uglify())
+         .pipe(header(banner, { pkg }))
         .pipe(gulp.dest(buildSrc+'/js/'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(notify("Found file: <%= file.relative %>!"));
